a06d5d09e3dbf39b255a695b5774ae354fe3e91f
JGheigvd <jules.grosse@heig-vd.ch>
2026-09-25 13:44:59 +0200

    Refactor main.cpp with comments and formatting

--- main.cpp
+++ main.cpp
@@ -2,21 +2,20 @@
 #include <cmath>
 
 int main() {
-
-    double const b = 3;
+// on définit les valeurs connus
+    double const b = 3; 
     double const L1 = 10;
     double const L2 = 6;
 
-    double a = L1-L2;
-    double L3t = (a*a)+(b*b);
-    double L3 = std::sqrt(L3t);
+    double a = L1-L2; // on trouve le coté inconnu du triangle
+    double L3 = std::sqrt((a*a)+(b*b)); //on trouve l'hypothénuse
 
-    double T1 = L2/5;
-    double T2 = L3/2;
+    double T1 = L2/5; // on trouve le temps de la partie route
+    double T2 = L3/2; // on trouve le temps de la partie rocheuse
 
-    double Ttot = T1 + T2;
+    double Ttot = T1 + T2; // on additione les deux temps
 
-    std::cout<<"Voici le temps total que va prendre le robot : "<<Ttot;
+    std::cout<<"Voici le temps total que va prendre le robot : "<<Ttot<<" heures"; //phrase de fin
 
 
     return 0;
